906393c6308abf6b466d5b2557be95dd1e8cd196
w1nt0r <j4n.winter@gmail.com>
2017-10-12 10:38:09 +0200

    added base layout for registration

---
+++ app/src/main/java/com/example/schef/gadgeothek/RegistrationFragment.java
@@ -0,0 +1,24 @@
+package com.example.schef.gadgeothek;
+
+
+import android.os.Bundle;
+import android.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class RegistrationFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_registration, container, false);
+        return root;
+    }
+
+}
