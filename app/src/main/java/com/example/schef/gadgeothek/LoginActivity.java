7b080545e8bfe5554c8de420786ff54c83947109
Gabriel Vlasek <gvlasek@bluewin.ch>
2017-10-16 10:09:25 +0200

    commited to prevent conflicts

--- app/src/main/java/com/example/schef/gadgeothek/LoginActivity.java
+++ app/src/main/java/com/example/schef/gadgeothek/LoginActivity.java
@@ -1,13 +1,63 @@
 package com.example.schef.gadgeothek;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.Cursor;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.view.accessibility.AccessibilityManager;
+import android.widget.Toast;
 
-public class LoginActivity extends AppCompatActivity {
+import com.example.schef.service.Callback;
+import com.example.schef.service.DBService;
+import com.example.schef.service.LibraryService;
+
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+    //String mail = "herr@doepfel.ch";
+    //String password = "123";
+    String serverAdress;
+    DBService db;
+    Intent gadgeothekIntent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        getFragmentManager().beginTransaction().add(R.id.frameLayout, new LoginFragment()).commit();
+        gadgeothekIntent = new Intent(this, GadgeothekActivity.class);
+        db = DBService.getDBService(this);
+        try{
+            db.insertNewConnection("TestServer5", "http://mge5.dev.ifs.hsr.ch/public");
+            db.insertNewConnection("TestServer4", "http://mge5.dev.ifs.hsr.ch/public");
+
+        }
+        catch (Exception ex){
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        //Cursor c = db.getConnections();
+       // c.moveToFirst();
+       // db.changeCurrentConnection(1);
+        try {
+            Toast.makeText(this, db.getCurrentServer(), Toast.LENGTH_LONG).show();
+        }catch (Exception ex){
+            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+            serverAdress = "http://mge4.dev.ifs.hsr.ch/public";
+        LibraryService.setServerAddress(serverAdress);
+
+
+    }
+
+    public void onClick(View view) {
+        //Toast toast;
+        String s = db.getCurrentServer();
+        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
+        //if(view.getId()== R.)
+        //Toast toast = Toast.makeText(getApplicationContext(), view.toString(), Toast.LENGTH_LONG);
+
+        //toast.show();
+        startActivity(gadgeothekIntent);
     }
 }
--- app/src/main/java/com/example/schef/gadgeothek/LoginFragment.java
+++ app/src/main/java/com/example/schef/gadgeothek/LoginFragment.java
@@ -1,20 +1,31 @@
 package com.example.schef.gadgeothek;
 
 import android.content.Context;
-import android.net.Uri;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class LoginFragment extends Fragment implements View.OnClickListener{
+import com.example.schef.service.Callback;
+import com.example.schef.service.DBService;
+import com.example.schef.service.LibraryService;
+
+public class LoginFragment extends Fragment implements View.OnClickListener {
     private View.OnClickListener activity;
+    private EditText mail;
+    private EditText password;
+    DBService db;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_login, container, false);
         root.findViewById(R.id.loginButton).setOnClickListener(this);
+        mail = (EditText) root.findViewById(R.id.emailEditText);
+        password = (EditText) root.findViewById(R.id.passwordEditText);
+        db = DBService.getDBService((Context)activity);
         return root;
     }
 
@@ -30,6 +41,55 @@ public class LoginFragment extends Fragment implements View.OnClickListener{
 
     @Override
     public void onClick(View view) {
-        activity.onClick(view);
+        String email = mail.getText().toString();
+        String passwd = password.getText().toString();
+       // db.insertNewConnection("TestServer5", "http://mge5.dev.ifs.hsr.ch/public");
+        final View v = view;
+        //db.changeCurrentConnection(1);
+        //db = DBService.getDBService((Context)activity);
+       //tring s = db.getCurrentServer();
+        //Toast.makeText((Context)activity, s, Toast.LENGTH_LONG).show();
+        try {
+            db.insertNewConnection("TestServer5", "http://mge5.dev.ifs.hsr.ch/public");
+            db.insertNewConnection("TestServer4", "http://mge5.dev.ifs.hsr.ch/public");
+
+        } catch (Exception ex) {
+            Toast.makeText((Context) activity, "1: " + ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        try {
+            //Toast.makeText((Context) activity, "2: " + Integer.toString(db.getCurrent()), Toast.LENGTH_LONG).show();
+
+            db.changeCurrentConnection(1);
+        } catch (Exception ex) {
+            Toast.makeText((Context) activity, "2: " + ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+
+        try {
+            String s = db.getCurrentServer();
+            Toast.makeText((Context) activity, s, Toast.LENGTH_LONG).show();
+        } catch (Exception ex) {
+            Toast.makeText((Context) activity, "3: " + ex.getMessage(), Toast.LENGTH_LONG).show();
+        }
+        /*db.changeCurrentConnection(0);
+       */
+        LibraryService.login(email, passwd, new Callback<Boolean>()
+        {
+            @Override
+            public void onCompletion(Boolean successful) {
+                if (successful) {
+                    activity.onClick(v);
+                } else {
+                    Toast toast = Toast.makeText((Context) activity, "Wrong password or username", Toast.LENGTH_LONG);
+                    toast.show();
+                }
+            }
+
+            @Override
+            public void onError(String message) {
+                Toast toast = Toast.makeText((Context) activity, message, Toast.LENGTH_LONG);
+                toast.show();
+            }
+        });
     }
 }
