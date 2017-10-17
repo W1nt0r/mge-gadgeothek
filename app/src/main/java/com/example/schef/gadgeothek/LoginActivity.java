129bb835c36436c6934c258867af1c93063ad664
Gabriel Vlasek <gvlasek@bluewin.ch>
2017-10-17 09:28:45 +0200

    merge conflicts...

--- app/src/main/java/com/example/schef/gadgeothek/LoginActivity.java
+++ app/src/main/java/com/example/schef/gadgeothek/LoginActivity.java
@@ -4,68 +4,54 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Toast;
 
 import com.example.schef.domain.ConnectionData;
+import com.example.schef.domain.Constants;
 import com.example.schef.domain.State;
 import com.example.schef.service.DBService;
 import com.example.schef.service.LibraryService;
 
 import java.util.Stack;
 
-//String mail = "herr@doepfel.ch";
-    //String password = "123";
-
 public class LoginActivity extends AppCompatActivity implements View.OnClickListener, ServerChanger {
-    DBService db;
-    Intent gadgeothekIntent;
     String serverAdress;
 
     private Stack<State> stateStack = new Stack<>();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        DBService.getDBService(this);
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-
+        DBService db = DBService.getDBService(this);
         getFragmentManager().beginTransaction().add(R.id.frameLayout, new LoginFragment()).commit();
-        gadgeothekIntent = new Intent(this, GadgeothekActivity.class);
-        db = DBService.getDBService(this);
-        try{
-            db.insertNewConnection("TestServer5", "http://mge5.dev.ifs.hsr.ch/public");
-            db.insertNewConnection("TestServer4", "http://mge5.dev.ifs.hsr.ch/public");
 
-        }
-        catch (Exception ex){
-            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
-        }
+
         try {
-            Toast.makeText(this, db.getCurrentServer(), Toast.LENGTH_LONG).show();
-        }catch (Exception ex){
-            Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
+            db.insertNewConnection("tokenServer1", "19", "123", "herr@doepfel.ch", "server4", "http://mge4.dev.ifs.hsr.ch/public");
+            db.insertNewConnection("tokenServer2", "191", "123", "herr@doepfel.ch", "server5", "http://mge5.dev.ifs.hsr.ch/public");
+
+        } catch (Exception ex) {
+            //Toast.makeText(this, ex.getMessage(), Toast.LENGTH_LONG).show();
         }
-            serverAdress = "http://mge4.dev.ifs.hsr.ch/public";
+
+        SharedPreferences settings = getSharedPreferences(Constants.SHARED_PREF, MODE_PRIVATE);
+        SharedPreferences.Editor editor = settings.edit();
+        editor.putInt(Constants.CONNECTIONDATA_ARGS, 1);
+        editor.commit();
+
+        serverAdress = "http://mge4.dev.ifs.hsr.ch/public";
         LibraryService.setServerAddress(serverAdress);
-
-
     }
 
     public void onClick(View view) {
-        //Toast toast;
-        String s = db.getCurrentServer();
-        Toast.makeText(this, s, Toast.LENGTH_LONG).show();
-        //if(view.getId()== R.)
-        //Toast toast = Toast.makeText(getApplicationContext(), view.toString(), Toast.LENGTH_LONG);
+        Toast.makeText(this, "Login successful", Toast.LENGTH_LONG);
 
-        //toast.show();
-        startActivity(gadgeothekIntent);
-
-        stateStack.push(State.SERVER_MANAGE);
-        showFragment(new ServerManageFragment(), null);
     }
 
     private void showFragment(Fragment fragment, Bundle args) {
@@ -88,7 +74,7 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
             finish();
         }
     }
-    
+
     @Override
     public void changeServer(ConnectionData server) {
 
