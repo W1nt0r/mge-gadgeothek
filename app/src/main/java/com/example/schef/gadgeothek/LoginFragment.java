e0e425e7ac9e4efb3a3e619fd670fdb3a5ed5ce2
Gabriel Vlasek <gvlasek@bluewin.ch>
2017-10-17 09:10:13 +0200

    updated loginfragment

--- app/src/main/java/com/example/schef/gadgeothek/LoginFragment.java
+++ app/src/main/java/com/example/schef/gadgeothek/LoginFragment.java
@@ -1,6 +1,8 @@
 package com.example.schef.gadgeothek;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,6 +11,7 @@ import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.example.schef.domain.ConnectionData;
 import com.example.schef.service.Callback;
 import com.example.schef.service.DBService;
 import com.example.schef.service.LibraryService;
@@ -25,7 +28,13 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
         root.findViewById(R.id.loginButton).setOnClickListener(this);
         mail = (EditText) root.findViewById(R.id.emailEditText);
         password = (EditText) root.findViewById(R.id.passwordEditText);
-        db = DBService.getDBService((Context)activity);
+        db = DBService.getDBService(null);
+        ConnectionData c = db.getCurrentConnection(getActivity().getBaseContext());
+
+        if(c!= null) {
+            mail.setText(c.getCustomermail());
+            password.setText(c.getPassword());
+        }
         return root;
     }
 
@@ -43,51 +52,22 @@ public class LoginFragment extends Fragment implements View.OnClickListener {
     public void onClick(View view) {
         String email = mail.getText().toString();
         String passwd = password.getText().toString();
-       // db.insertNewConnection("TestServer5", "http://mge5.dev.ifs.hsr.ch/public");
         final View v = view;
-        //db.changeCurrentConnection(1);
-        //db = DBService.getDBService((Context)activity);
-       //tring s = db.getCurrentServer();
-        //Toast.makeText((Context)activity, s, Toast.LENGTH_LONG).show();
-        try {
-            db.insertNewConnection("TestServer5", "http://mge5.dev.ifs.hsr.ch/public");
-            db.insertNewConnection("TestServer4", "http://mge5.dev.ifs.hsr.ch/public");
-
-        } catch (Exception ex) {
-            Toast.makeText((Context) activity, "1: " + ex.getMessage(), Toast.LENGTH_LONG).show();
-        }
-
-        try {
-            //Toast.makeText((Context) activity, "2: " + Integer.toString(db.getCurrent()), Toast.LENGTH_LONG).show();
-
-            db.changeCurrentConnection(1);
-        } catch (Exception ex) {
-            Toast.makeText((Context) activity, "2: " + ex.getMessage(), Toast.LENGTH_LONG).show();
-        }
-
-        try {
-            String s = db.getCurrentServer();
-            Toast.makeText((Context) activity, s, Toast.LENGTH_LONG).show();
-        } catch (Exception ex) {
-            Toast.makeText((Context) activity, "3: " + ex.getMessage(), Toast.LENGTH_LONG).show();
-        }
-        /*db.changeCurrentConnection(0);
-       */
         LibraryService.login(email, passwd, new Callback<Boolean>()
         {
             @Override
             public void onCompletion(Boolean successful) {
                 if (successful) {
-                    activity.onClick(v);
+                    Toast.makeText(getActivity().getBaseContext(), "login successful", Toast.LENGTH_LONG).show();
+                   //click listener
                 } else {
-                    Toast toast = Toast.makeText((Context) activity, "Wrong password or username", Toast.LENGTH_LONG);
+                    Toast toast = Toast.makeText(getActivity().getBaseContext() , "Wrong password or username", Toast.LENGTH_LONG);
                     toast.show();
                 }
             }
-
             @Override
             public void onError(String message) {
-                Toast toast = Toast.makeText((Context) activity, message, Toast.LENGTH_LONG);
+                Toast toast = Toast.makeText(getActivity().getBaseContext(), message, Toast.LENGTH_LONG);
                 toast.show();
             }
         });
