1ce042c226a3455cf0dc44a5ec57cbfed0b4402a
w1nt0r <j4n.winter@gmail.com>
2017-10-19 11:05:25 +0200

    fixed registration

--- app/src/main/java/com/example/schef/gadgeothek/RegistrationFragment.java
+++ app/src/main/java/com/example/schef/gadgeothek/RegistrationFragment.java
@@ -82,50 +82,73 @@ public class RegistrationFragment extends Fragment implements View.OnClickListen
 
     @Override
     public void onClick(View view) {
-        showLoadingScreen();
         final String name = nameField.getText().toString();
         final String mail = mailField.getText().toString();
         final String matrikelnr = matrikelField.getText().toString();
         final String password = passwordField.getText().toString();
         final String passwordRep = passwordRepField.getText().toString();
+        boolean failure = false;
 
-        if(name.equals("")) nameField.setError("Namens-Feld ist leer");
+        if(name.equals("")) {
+            failure = true;
+            nameField.setError("Namens-Feld ist leer");
+        }
 
-        if(mail.equals("")) mailField.setError("Mail-Adressen-Feld ist leer");
+        if(mail.equals("")) {
+            failure = true;
+            mailField.setError("Mail-Adressen-Feld ist leer");
+        }
 
-        if(matrikelnr.equals("")) matrikelField.setError("Matrikelnummern-Feld ist leer");
+        if(matrikelnr.equals("")) {
+            failure = true;
+            matrikelField.setError("Matrikelnummern-Feld ist leer");
+        }
 
-        if(password.equals("")) passwordField.setError("Passwort-Feld ist leer");
+        if(password.equals("")) {
+            failure = true;
+            passwordField.setError("Passwort-Feld ist leer");
+        }
 
-        if(passwordRep.equals("")) passwordRepField.setError("Passwort-Wiederhlungs-Feld ist leer");
+        if(passwordRep.equals("")) {
+            failure = true;
+            passwordRepField.setError("Passwort-Wiederhlungs-Feld ist leer");
+        }
 
-        if(!password.equals(passwordRep)) passwordRepField.setError("Die eingegebenen Passwörter stimmen nicht miteinander überein");
+        if(!password.equals(passwordRep)) {
+            failure = true;
+            passwordRepField.setError("Die eingegebenen Passwörter stimmen nicht miteinander überein");
+        }
 
-        LibraryService.register(mail, password, name, matrikelnr, new Callback<Boolean>() {
-            @Override
-            public void onCompletion(Boolean input) {
-                //insert into db, return to other fragment
-                if(Constants.DEV) Log.d(getString(R.string.app_name), "Registration State: " + input);
+        if(!failure) {
+            showLoadingScreen();
 
-                if(input) {
-                    Toast.makeText(getActivity(), "Die Registierung wurde erfolgreich durchgeführt", Toast.LENGTH_SHORT).show();
-                } else {
-                    Toast.makeText(getActivity(), "Die Registierung konnte nicht erfolgreich durchgeführt werden", Toast.LENGTH_SHORT).show();
+            LibraryService.register(mail, password, name, matrikelnr, new Callback<Boolean>() {
+                @Override
+                public void onCompletion(Boolean input) {
+                    //insert into db, return to other fragment
+                    if (Constants.DEV)
+                        Log.d(getString(R.string.app_name), "Registration State: " + input);
+
+                    if (input) {
+                        Toast.makeText(getActivity(), "Die Registierung wurde erfolgreich durchgeführt", Toast.LENGTH_SHORT).show();
+                    } else {
+                        Toast.makeText(getActivity(), "Die Registierung konnte nicht erfolgreich durchgeführt werden", Toast.LENGTH_SHORT).show();
+                    }
+
+                    connectionData.setCustomermail(mail);
+                    connectionData.setPassword(password);
+                    activity.register(connectionData);
                 }
 
-                connectionData.setCustomermail(mail);
-                connectionData.setPassword(password);
-                activity.register(connectionData);
-            }
+                @Override
+                public void onError(String message) {
+                    if (Constants.DEV) Log.d(getString(R.string.app_name), message);
+                    String errmsg = "Die Registrierung konnte nicht durchgeführt werden! Bitte versuchen Sie es später noch einmal";
 
-            @Override
-            public void onError(String message) {
-                if(Constants.DEV) Log.d(getString(R.string.app_name), message);
-                String errmsg = "Die Registrierung konnte nicht durchgeführt werden! Bitte versuchen Sie es später noch einmal";
-
-                Toast.makeText(getActivity(), errmsg, Toast.LENGTH_LONG).show();
-            }
-        });
+                    Toast.makeText(getActivity(), errmsg, Toast.LENGTH_LONG).show();
+                }
+            });
+        }
     }
 
     private void showLoadingScreen() {
